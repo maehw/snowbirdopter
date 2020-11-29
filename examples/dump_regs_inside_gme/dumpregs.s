45b545ccad604f70096ca1dfdb4a42b471ebf507
maehw <matthiaz.weber@gmx.de>
2020-11-30 00:16:08 +0100

    Fixed dumpregs, added demo for '_still_works'

--- examples/dump_regs_inside_gme/dumpregs.s
+++ examples/dump_regs_inside_gme/dumpregs.s
@@ -11,6 +11,9 @@ _still_works:
     pop {r4, r5, r6, r7, r8, r9, sl, pc}
 
 _dump_regs:
+    @ https://stackoverflow.com/questions/47109767/push-and-pop-order-in-arm/47109995:
+    @ "When you PUSH or POP a bunch of registers, they always go into memory in the same relative positions, regardless of direction.
+    @  The lowest-numberd register is stored at and loaded from the lowest address."
     push {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, lr} @ r10/sl, r11/fp, r12/ip, r14/lr; do not store r13/sp and r15/pc on the stack!
 
     mov r4, sp
@@ -27,74 +30,29 @@ _dump_regs:
     @ init the UART
     bl 0x10D8
 
-    @ dump r15/pc from within this function
+
+    @ dump stored r0
     mov r0, sp
     bl 0x159C
 
-    mov r0, #49
-    bl 0x1504
-
-    mov r0, #53
+    mov r0, #48
     bl 0x1504
 
     mov r0, #58
     bl 0x1504
 
-    mov r0, pc @ offset as we are in the middle of this function
+    ldr r0, [r4, #0]
     bl 0x1810
 
     mov r0, #44
     bl 0x1504
 
 
-    @ dump stored r14/lr
+    @ dump stored r1
     mov r0, sp
     bl 0x159C
 
     mov r0, #49
-    bl 0x1504
-
-    mov r0, #52
-    bl 0x1504
-
-    mov r0, #58
-    bl 0x1504
-
-    ldr r0, [r4]
-    bl 0x1810
-
-    mov r0, #44
-    bl 0x1504
-
-
-    @ dump r13/sp from within this function
-    mov r0, sp
-    bl 0x159C
-
-    mov r0, #49
-    bl 0x1504
-
-    mov r0, #51
-    bl 0x1504
-
-    mov r0, #58
-    bl 0x1504
-
-    mov r0, r4 @ sp offset by number of stored registers after function epilogue
-    bl 0x1810
-
-    mov r0, #44
-    bl 0x1504
-
-
-    @ dump stored r12
-    mov r0, sp
-    bl 0x159C
-
-    mov r0, #49
-    bl 0x1504
-
-    mov r0, #50
     bl 0x1504
 
     mov r0, #58
@@ -107,20 +65,136 @@ _dump_regs:
     bl 0x1504
 
 
-    @ dump stored r11
+    @ dump stored r2
     mov r0, sp
     bl 0x159C
 
-    mov r0, #49
-    bl 0x1504
-
-    mov r0, #49
+    mov r0, #50
     bl 0x1504
 
     mov r0, #58
     bl 0x1504
 
     ldr r0, [r4, #8]
+    bl 0x1810
+
+    mov r0, #44
+    bl 0x1504
+
+
+    @ dump stored r3
+    mov r0, sp
+    bl 0x159C
+
+    mov r0, #51
+    bl 0x1504
+
+    mov r0, #58
+    bl 0x1504
+
+    ldr r0, [r4, #12]
+    bl 0x1810
+
+    mov r0, #44
+    bl 0x1504
+
+
+    @ dump stored r4
+    mov r0, sp
+    bl 0x159C
+
+    mov r0, #52
+    bl 0x1504
+
+    mov r0, #58
+    bl 0x1504
+
+    ldr r0, [r4, #16]
+    bl 0x1810
+
+    mov r0, #44
+    bl 0x1504
+
+
+    @ dump stored r5
+    mov r0, sp
+    bl 0x159C
+
+    mov r0, #53
+    bl 0x1504
+
+    mov r0, #58
+    bl 0x1504
+
+    ldr r0, [r4, #20]
+    bl 0x1810
+
+    mov r0, #44
+    bl 0x1504
+
+
+    @ dump stored r6
+    mov r0, sp
+    bl 0x159C
+
+    mov r0, #54
+    bl 0x1504
+
+    mov r0, #58
+    bl 0x1504
+
+    ldr r0, [r4, #24]
+    bl 0x1810
+
+    mov r0, #44
+    bl 0x1504
+
+
+    @ dump stored r7
+    mov r0, sp
+    bl 0x159C
+
+    mov r0, #55
+    bl 0x1504
+
+    mov r0, #58
+    bl 0x1504
+
+    ldr r0, [r4, #28]
+    bl 0x1810
+
+    mov r0, #44
+    bl 0x1504
+
+
+    @ dump stored r8
+    mov r0, sp
+    bl 0x159C
+
+    mov r0, #56
+    bl 0x1504
+
+    mov r0, #58
+    bl 0x1504
+
+    ldr r0, [r4, #32]
+    bl 0x1810
+
+    mov r0, #44
+    bl 0x1504
+
+
+    @ dump stored r9
+    mov r0, sp
+    bl 0x159C
+
+    mov r0, #57
+    bl 0x1504
+
+    mov r0, #58
+    bl 0x1504
+
+    ldr r0, [r4, #36]
     bl 0x1810
 
     mov r0, #44
@@ -140,125 +214,6 @@ _dump_regs:
     mov r0, #58
     bl 0x1504
 
-    ldr r0, [r4, #12]
-    bl 0x1810
-
-    mov r0, #44
-    bl 0x1504
-
-
-    @ dump stored r9
-    mov r0, sp
-    bl 0x159C
-
-    mov r0, #57
-    bl 0x1504
-
-    mov r0, #58
-    bl 0x1504
-
-    ldr r0, [r4, #16]
-    bl 0x1810
-
-    mov r0, #44
-    bl 0x1504
-
-
-    @ dump stored r8
-    mov r0, sp
-    bl 0x159C
-
-    mov r0, #56
-    bl 0x1504
-
-    mov r0, #58
-    bl 0x1504
-
-    ldr r0, [r4, #20]
-    bl 0x1810
-
-    mov r0, #44
-    bl 0x1504
-
-
-    @ dump stored r7
-    mov r0, sp
-    bl 0x159C
-
-    mov r0, #55
-    bl 0x1504
-
-    mov r0, #58
-    bl 0x1504
-
-    ldr r0, [r4, #24]
-    bl 0x1810
-
-    mov r0, #44
-    bl 0x1504
-
-
-    @ dump stored r6
-    mov r0, sp
-    bl 0x159C
-
-    mov r0, #54
-    bl 0x1504
-
-    mov r0, #58
-    bl 0x1504
-
-    ldr r0, [r4, #28]
-    bl 0x1810
-
-    mov r0, #44
-    bl 0x1504
-
-
-    @ dump stored r5
-    mov r0, sp
-    bl 0x159C
-
-    mov r0, #53
-    bl 0x1504
-
-    mov r0, #58
-    bl 0x1504
-
-    ldr r0, [r4, #32]
-    bl 0x1810
-
-    mov r0, #44
-    bl 0x1504
-
-
-    @ dump stored r4
-    mov r0, sp
-    bl 0x159C
-
-    mov r0, #52
-    bl 0x1504
-
-    mov r0, #58
-    bl 0x1504
-
-    ldr r0, [r4, #36]
-    bl 0x1810
-
-    mov r0, #44
-    bl 0x1504
-
-
-    @ dump stored r3
-    mov r0, sp
-    bl 0x159C
-
-    mov r0, #51
-    bl 0x1504
-
-    mov r0, #58
-    bl 0x1504
-
     ldr r0, [r4, #40]
     bl 0x1810
 
@@ -266,11 +221,14 @@ _dump_regs:
     bl 0x1504
 
 
-    @ dump stored r2
+    @ dump stored r11
     mov r0, sp
     bl 0x159C
 
-    mov r0, #50
+    mov r0, #49
+    bl 0x1504
+
+    mov r0, #49
     bl 0x1504
 
     mov r0, #58
@@ -283,11 +241,14 @@ _dump_regs:
     bl 0x1504
 
 
-    @ dump stored r1
+    @ dump stored r12
     mov r0, sp
     bl 0x159C
 
     mov r0, #49
+    bl 0x1504
+
+    mov r0, #50
     bl 0x1504
 
     mov r0, #58
@@ -300,11 +261,34 @@ _dump_regs:
     bl 0x1504
 
 
-    @ dump stored r0
+    @ dump stored r13/sp
     mov r0, sp
     bl 0x159C
 
-    mov r0, #48
+    mov r0, #49
+    bl 0x1504
+
+    mov r0, #51
+    bl 0x1504
+
+    mov r0, #58
+    bl 0x1504
+
+    mov r0, r4 @ use stack pointer (sp) stored after epilog
+    bl 0x1810
+
+    mov r0, #44
+    bl 0x1504
+
+
+    @ dump stored r14/lr
+    mov r0, sp
+    bl 0x159C
+
+    mov r0, #49
+    bl 0x1504
+
+    mov r0, #52
     bl 0x1504
 
     mov r0, #58
@@ -315,8 +299,28 @@ _dump_regs:
 
     mov r0, #44
     bl 0x1504
-    
-    
+
+
+    @ dump current r15/pc
+    mov r0, sp
+    bl 0x159C
+
+    mov r0, #49
+    bl 0x1504
+
+    mov r0, #53
+    bl 0x1504
+
+    mov r0, #58
+    bl 0x1504
+
+    mov r0, pc
+    bl 0x1810
+
+    mov r0, #44
+    bl 0x1504
+
+
     mov r0, #10
     bl 0x1504
 
--- examples/dump_regs_inside_gme/main.c
+++ examples/dump_regs_inside_gme/main.c
@@ -1,10 +1,25 @@
 #include "tiptoi.h"
 
-extern void _still_works(unsigned int a, unsigned int b, unsigned int c, unsigned int d);
+extern void _still_works(unsigned int* a, unsigned int* b, unsigned char* c, unsigned int d);
+
+unsigned int myptrs[16];
+unsigned char mychars[3570];
 
 void main()
 {
+    int k;
+    for(k = 0; k < sizeof(myptrs)/sizeof(myptrs[0]); k++)
+    {
+        myptrs[k] = 0;
+    }
+    myptrs[13] = &mychars[0]; // point to start of valid byte array; as this pointer will be dereferenced by _still_works
+
+    for(k = 0; k < sizeof(mychars)/sizeof(mychars[0]); k++)
+    {
+        mychars[k] = 'w'; // pre-initialize byte array to 'w' / 0x77
+    }
+
     // call function with four arguments; first argument is stored in r0, fourth argument in r3
-	_still_works(0x11234567, 0x22DEADBF, 0x33BAD1DE, 0x44C0FFEE);
+	_still_works(myptrs, myptrs[13], &mychars[3564], 0xDEADBEEF);
 }
 
