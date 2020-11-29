28bacecf503612bc49e4796a8cd0f577bdc1c4a0
maehw <matthiaz.weber@gmx.de>
2020-11-30 00:18:45 +0100

    Fixed types of pointers

--- examples/dump_regs_inside_gme/main.c
+++ examples/dump_regs_inside_gme/main.c
@@ -2,7 +2,7 @@
 
 extern void _still_works(unsigned int* a, unsigned int* b, unsigned char* c, unsigned int d);
 
-unsigned int myptrs[16];
+unsigned int* myptrs[16];
 unsigned char mychars[3570];
 
 void main()
@@ -10,9 +10,9 @@ void main()
     int k;
     for(k = 0; k < sizeof(myptrs)/sizeof(myptrs[0]); k++)
     {
-        myptrs[k] = 0;
+        myptrs[k] = (unsigned int*)0;
     }
-    myptrs[13] = &mychars[0]; // point to start of valid byte array; as this pointer will be dereferenced by _still_works
+    myptrs[13] = (unsigned int*)&mychars[0]; // point to start of valid byte array; as this pointer will be dereferenced by _still_works
 
     for(k = 0; k < sizeof(mychars)/sizeof(mychars[0]); k++)
     {
@@ -20,6 +20,6 @@ void main()
     }
 
     // call function with four arguments; first argument is stored in r0, fourth argument in r3
-	_still_works(myptrs, myptrs[13], &mychars[3564], 0xDEADBEEF);
+	_still_works(myptrs[0], myptrs[13], &mychars[3564], 0xDEADBEEF);
 }
 
