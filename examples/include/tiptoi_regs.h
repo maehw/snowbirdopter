e6b38f9ade68fe6d00b71cdef36ae54f9dd4c1f3
maehw <matthiaz.weber@gmx.de>
2020-12-21 17:01:26 +0100

    Added findings about I/O multiplexing

--- examples/include/tiptoi_regs.h
+++ examples/include/tiptoi_regs.h
@@ -3,6 +3,7 @@
 
 // The following hardware registers have been found and (mostly) understood what they are used for
 volatile int* const pREG_BOOT_MODE = (int*)0x04000054;
+volatile int* const pREG_SHARE_PIN_CTRL = (int*)0x4000074; /* value in UART boot is read as 0x00005001; it seems that bit 0 is responsible for UART TX */
 
 #endif /* __TIPTOI_REGS_H__ */
 
