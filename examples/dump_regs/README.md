2dc14f5ea8937a3cc8ce055a0a1f98aa18a00bfa
maehw <matthiaz.weber@gmx.de>
2020-12-28 15:59:54 +0100

    Used own UART functions, updated README.md

--- examples/dump_regs/README.md
+++ examples/dump_regs/README.md
@@ -1,11 +1,28 @@
 # Build and analysis steps
 
-(work in progress)
+see Makefile, the following `arm-none-eabi-*` tools are used:
+
+* as,
+* ld,
+* objdump and
+* objcopy
 
 
-```
-arm-none-eabi-as dumpregs.s -o dumpregs.o
-arm-none-eabi-ld dumpregs.o -o dumpregs.elf
-arm-none-eabi-objdump --architecture=arm -D dumpregs.elf | less
+
+# Execute
+
+Enter UART boot mode.
+
+Execute the binary using snowbirdopter:
+
+```$ python3 snowbirdopter.py -p /dev/ttyUSB0 -c exec -f ./examples/dump_regs/out.bin -a 08010000```
+
+The command line and the output should look similar to:
+
+```$ python3 snowbirdopter.py -p /dev/ttyUSB0 -c exec -f ./examples/dump_regs/out.bin -a 08010000
+[DEBUG] payload length: 2312, overall download length: 2318
+[INFO] Loading binfile succeeded.
+[DEBUG] go to address 0x08010000
+[INFO] Read from serial: b'Hello Tiptoi! Hello reg r0:00000001,reg r1:0802ee84,reg r2:04036004,reg r3:00000000,reg r4:db7997e3,reg r5:45bd17df,reg r6:06be7e3d,reg r7:1b66cc1f,reg r8:57cff67f,reg r9:1bfdeccf,reg r10:f7c6dbe1,reg r11:0802ee80,reg r12:0802ee74,reg r13:0802ee3c,reg r14:080100c0,reg r15:080107c8,REG_SHARE_PIN_CTRL:00005001,REG_GPIO_DIR_1:ffffffff,REG_GPIO_OUT_1:00000000,REG_GPIO_DIR_2:ffffffff,REG_GPIO_OUT_2:00000000,REG_TBD:02f00024,REG_UART_CFG1:00200619,REG_UART_TXRX_BUF_THRESHOLD:00000000,world!\n'
 ```
 
--- examples/dump_regs/main.c
+++ examples/dump_regs/main.c
@@ -2,8 +2,13 @@
 
 extern void _dump_regs(void);
 
+int my_uart_init(void);
+int my_uart_putc(int c);
+
 void main()
 {
+    int nRegVal = 0;
+
 	// Define a string on the stack
 	char* pcString = "Hello Tiptoi!";
 
@@ -20,14 +25,91 @@ void main()
 
 	_dump_regs();
 
+    // Dump some more registers
+    bootrom_uart_puts("REG_SHARE_PIN_CTRL:");
+    nRegVal = *pREG_SHARE_PIN_CTRL;
+    bootrom_uart_put_num2hex(nRegVal);
+	bootrom_uart_putc(',');
+	
+	//asm("mov pc, #0x0;");
+
+    bootrom_uart_puts("REG_GPIO_DIR_1:");
+    nRegVal = *pREG_GPIO_DIR_1;
+    bootrom_uart_put_num2hex(nRegVal);
+	bootrom_uart_putc(',');
+
+    bootrom_uart_puts("REG_GPIO_OUT_1:");
+    nRegVal = *pREG_GPIO_OUT_1;
+    bootrom_uart_put_num2hex(nRegVal);
+	bootrom_uart_putc(',');
+
+    bootrom_uart_puts("REG_GPIO_DIR_2:");
+    nRegVal = *pREG_GPIO_DIR_2;
+    bootrom_uart_put_num2hex(nRegVal);
+	bootrom_uart_putc(',');
+
+    bootrom_uart_puts("REG_GPIO_OUT_2:");
+    nRegVal = *pREG_GPIO_OUT_2;
+    bootrom_uart_put_num2hex(nRegVal);
+	bootrom_uart_putc(',');
+
+    bootrom_uart_puts("REG_TBD:");
+    nRegVal = *pREG_TBD;
+    bootrom_uart_put_num2hex(nRegVal);
+	bootrom_uart_putc(',');
+
+    bootrom_uart_puts("REG_UART_CFG1:");
+    nRegVal = *pREG_UART_CFG1;
+    bootrom_uart_put_num2hex(nRegVal);
+	bootrom_uart_putc(',');
+
+    bootrom_uart_puts("REG_UART_TXRX_BUF_THRESHOLD:");
+    nRegVal = *pREG_UART_TXRX_BUF_THRESHOLD;
+    bootrom_uart_put_num2hex(nRegVal);
+	bootrom_uart_putc(',');
+
+    my_uart_init();
+
     // Print the string character by character (i.e. byte by byte) to the serial
-	bootrom_uart_putc('w');
+    my_uart_putc('w');
+	//bootrom_uart_putc('w');
+
 	bootrom_uart_putc('o');
-	bootrom_uart_putc('r');
+
+    my_uart_putc('r');
+	//bootrom_uart_putc('r');
+
 	bootrom_uart_putc('l');
-	bootrom_uart_putc('d');
+
+	my_uart_putc('d');
+	//bootrom_uart_putc('d');
+
 	bootrom_uart_putc('!');
 	bootrom_uart_putc('\n');
-	
+}
+
+int my_uart_init(void)
+{
+    *pREG_SHARE_PIN_CTRL = *pREG_SHARE_PIN_CTRL | 1;
+    *pREG_TBD = *pREG_TBD & 0xfc0fffff;
+    *pREG_TBD = *pREG_TBD | 0x2f00020;
+    *pREG_UART_CFG1 = 0x30200619;
+    *pREG_UART_TXRX_BUF_THRESHOLD = 0;
+    return 0;
+}
+
+int my_uart_putc(int c)
+{
+    int a;
+
+    *((int*)0x0802FA80) = c;
+    *((int*)0x0802FABC) = 0;
+    *pREG_UART_CFG1 |= 0x10000000; // set .TX_STA_CLR
+    *pREG_UART_CFG2 = 0x00010010; // set .TX_BYT_CNT_VLD and .TX_BYT_CNT
+    do
+    {
+    } while( *pREG_UART_DATA_CFG & 0x1FFF ); // ignore 19 MSBit, leave 13 LSBit
+
+    return 1;
 }
 
