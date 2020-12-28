49b6b0ab272927c6c8d5aad36464b40e76756979
maehw <matthiaz.weber@gmx.de>
2020-12-28 16:12:54 +0100

    Replaced some UART boot ROM functions with own code

--- examples/dump_regs/main.c
+++ examples/dump_regs/main.c
@@ -2,93 +2,65 @@
 
 extern void _dump_regs(void);
 
-int my_uart_init(void);
-int my_uart_putc(int c);
+static int uart_init(void);
+static int uart_putc(int c);
+static void uart_puts(char* pcString);
 
 void main()
 {
     int nRegVal = 0;
 
-	// Define a string on the stack
-	char* pcString = "Hello Tiptoi!";
-
-    // Print the string
-	bootrom_uart_puts(pcString);
-
-	bootrom_uart_putc(' ');
-	bootrom_uart_putc('H');
-	bootrom_uart_putc('e');
-	bootrom_uart_putc('l');
-	bootrom_uart_putc('l');
-	bootrom_uart_putc('o');
-	bootrom_uart_putc(' ');
+    uart_init();
+	uart_puts("CPU register dump: ");
 
 	_dump_regs();
 
+	uart_putc(',');
+
     // Dump some more registers
-    bootrom_uart_puts("REG_SHARE_PIN_CTRL:");
+    uart_puts("REG_SHARE_PIN_CTRL:");
     nRegVal = *pREG_SHARE_PIN_CTRL;
     bootrom_uart_put_num2hex(nRegVal);
-	bootrom_uart_putc(',');
+	uart_putc(',');
 	
-	//asm("mov pc, #0x0;");
-
     bootrom_uart_puts("REG_GPIO_DIR_1:");
     nRegVal = *pREG_GPIO_DIR_1;
     bootrom_uart_put_num2hex(nRegVal);
-	bootrom_uart_putc(',');
+	uart_putc(',');
 
     bootrom_uart_puts("REG_GPIO_OUT_1:");
     nRegVal = *pREG_GPIO_OUT_1;
     bootrom_uart_put_num2hex(nRegVal);
-	bootrom_uart_putc(',');
+	uart_putc(',');
 
     bootrom_uart_puts("REG_GPIO_DIR_2:");
     nRegVal = *pREG_GPIO_DIR_2;
     bootrom_uart_put_num2hex(nRegVal);
-	bootrom_uart_putc(',');
+	uart_putc(',');
 
     bootrom_uart_puts("REG_GPIO_OUT_2:");
     nRegVal = *pREG_GPIO_OUT_2;
     bootrom_uart_put_num2hex(nRegVal);
-	bootrom_uart_putc(',');
+	uart_putc(',');
 
     bootrom_uart_puts("REG_TBD:");
     nRegVal = *pREG_TBD;
     bootrom_uart_put_num2hex(nRegVal);
-	bootrom_uart_putc(',');
+	uart_putc(',');
 
     bootrom_uart_puts("REG_UART_CFG1:");
     nRegVal = *pREG_UART_CFG1;
     bootrom_uart_put_num2hex(nRegVal);
-	bootrom_uart_putc(',');
+	uart_putc(',');
 
     bootrom_uart_puts("REG_UART_TXRX_BUF_THRESHOLD:");
     nRegVal = *pREG_UART_TXRX_BUF_THRESHOLD;
     bootrom_uart_put_num2hex(nRegVal);
-	bootrom_uart_putc(',');
 
-    my_uart_init();
-
-    // Print the string character by character (i.e. byte by byte) to the serial
-    my_uart_putc('w');
-	//bootrom_uart_putc('w');
-
-	bootrom_uart_putc('o');
-
-    my_uart_putc('r');
-	//bootrom_uart_putc('r');
-
-	bootrom_uart_putc('l');
-
-	my_uart_putc('d');
-	//bootrom_uart_putc('d');
-
-	bootrom_uart_putc('!');
-	bootrom_uart_putc('\n');
+	uart_putc('\n');
 }
 
-int my_uart_init(void)
+int uart_init(void)
 {
     *pREG_SHARE_PIN_CTRL = *pREG_SHARE_PIN_CTRL | 1;
     *pREG_TBD = *pREG_TBD & 0xfc0fffff;
@@ -98,7 +70,7 @@ int my_uart_init(void)
     return 0;
 }
 
-int my_uart_putc(int c)
+int uart_putc(int c)
 {
     int a;
 
@@ -111,5 +83,14 @@ int my_uart_putc(int c)
     } while( *pREG_UART_DATA_CFG & 0x1FFF ); // ignore 19 MSBit, leave 13 LSBit
 
     return 1;
+}
+
+void uart_puts(char* pcString)
+{
+    char* pcChar = pcString;
+    while(*pcChar != '\0')
+    {
+        uart_putc(*pcChar++);
+    }
 }
 
