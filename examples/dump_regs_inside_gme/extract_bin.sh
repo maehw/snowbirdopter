58b1515acee1591717b9163486855e59c49594e0
maehw <matthiaz.weber@gmx.de>
2021-01-12 21:18:49 +0100

    Advanced half-automated binary extraction and disassembly

--- examples/dump_regs_inside_gme/extract_bin.sh
+++ examples/dump_regs_inside_gme/extract_bin.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
-# Extract the game's ARM binary using the outputs (offset + size) from analyze_gme.sh
-dd status=none iflag=skip_bytes,count_bytes skip="$((0x0AF5B273))" if="game.gme" count=22244 of="game.bin"
+# parse CSV file, extract binary for every row entry
+idx=0
+while IFS=, read -r offset size
+do
+    # Extract the game's ARM binary using the outputs (offset + size) from analyze_gme.sh
+    dd status=none iflag=skip_bytes,count_bytes skip="$((offset))" if="game.gme" count="$((size))" of="game$((idx)).bin"
 
-# Disassemble the binary and make assembly code available in separate file 'game.s'
-# -m machine|--architecture=machine: arm
-# -D|--disassemble-all
-# -b bfdname|--target=bfdname: binary
-# may consider using --no-show-raw-insn!
-arm-none-eabi-objdump -m arm -D -b binary game.bin > game.s
+    # Disassemble the binary and make assembly code available in separate file 'game.s'
+    # -m machine|--architecture=machine: arm
+    # -D|--disassemble-all
+    # -b bfdname|--target=bfdname: binary
+    # may consider using --no-show-raw-insn!
+    arm-none-eabi-objdump -m arm -D -b binary "game$((idx)).bin" > "game$((idx)).s"
+
+    idx=$((idx + 1))
+done < binaries.csv
+
+exit
+
+
 
