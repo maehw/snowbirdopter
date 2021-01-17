15b3064d6cea682983d0a4ab40aec6b7c4aba9cf
maehw <matthiaz.weber@gmx.de>
2021-01-17 13:28:38 +0100

    Fixed download script when gme_name contains spaces

--- examples/dump_regs_inside_gme/download_gme.sh
+++ examples/dump_regs_inside_gme/download_gme.sh
@@ -3,11 +3,12 @@
 gme_name=`cat gme_name`
 echo "[INFO] gme_name='$gme_name'"
 
-url=https://ssl-static.ravensburger.de/db/applications/$gme_name
+url="https://ssl-static.ravensburger.de/db/applications/$gme_name"
 echo "[INFO] url='$url'"
 
 # Download original gme file
-wget $url 
+echo "[INFO] wget \"$url\""
+wget "$url"
 
 # Copy it so that the next scripts can work on the more generic file name 'game.gme';
 # keep original file
